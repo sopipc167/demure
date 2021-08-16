0adc0145b41a02c56678856664c92738ff6b070b
sopipc167 <62834431+sopipc167@users.noreply.github.com>
2021-08-16 15:29:17 +0900

    Update documentControl.js

--- js/documentControl.js
+++ js/documentControl.js
@@ -95,7 +95,7 @@ function applyButton(ipt){
 	newbtn.setAttribute("id","val");
 	newbtn.innerText=val
 	document.getElementById("btnpanel").appendChild(newbtn);
-	ipt.style.diplay="none";
+	ipt.style.display="none";
 	}
 function primFunction(btn){
 	releaseButton();
