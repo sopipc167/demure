71cb6a062b2f9a620ffb98422d582512dcd72cd3
sopipc167 <62834431+sopipc167@users.noreply.github.com>
2021-08-16 15:08:38 +0900

    Update documentControl.js

--- js/documentControl.js
+++ js/documentControl.js
@@ -87,7 +87,7 @@ var discriptionText={}
 	discriptionText["프리모르스키 아쿠아리움"]="수족관이다";
 					
 function applyLoction(){
-	getElementById("ipt").style.display="inline";
+	document.getElementById("ipt").style.display="inline";
 	}
 function applyButton(ipt){
 	newbtn=document.createElement("button");
